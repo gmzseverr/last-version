2f6809dd14b31285d9952f0397f12a12e49e3a68
gmzseverr <161755952+gmzseverr@users.noreply.github.com>
2024-04-19 15:20:34 +0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'sdz5ap',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
